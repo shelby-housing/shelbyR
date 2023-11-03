d231edc85c6ae19fc0dda520baae353cb37f6040
Sarah Johnson <57082282+sj-io@users.noreply.github.com>
2023-11-03 10:26:44 -0500

    added clean_string so read_bids will work

--- R/read_bids.R
+++ R/read_bids.R
@@ -87,3 +87,40 @@ read_bid_details <- function(filepath) {
            contractor_id,
            bid_amount)
 }
+
+#' Clean a string to Title Case & string squish
+#'
+#' @param x A string to clean
+#' @param punct Keep punctuation?
+#' @param spaces Keep spaces?
+#' @param case "lower", "upper", "title", or "title up" for title case with three letter segments in uppercase (useful for many abbreviations).
+#'
+#' @return A Title Case String.
+#'
+#' @import stringr
+clean_string <- function(x, case = FALSE, punct = TRUE, spaces = TRUE) {
+  if (punct == FALSE) {
+    x <- str_replace_all(x, "[:punct:]|[:symbol:]", " ")
+  } else {
+    x
+  }
+
+  if (case == "title") {
+    x <- str_to_title(x)
+  } else if (case == "title up") {
+    x <- str_to_title(x)
+    x <- str_replace_all(x, "\\b[\\w[:punct:]]{3}\\b", toupper)
+  } else if (case == "lower") {
+    x <- str_to_lower(x)
+  } else if (case == "upper") {
+    x <- str_to_upper(x)
+  } else {
+    x
+  }
+
+  if (spaces == FALSE) {
+    x <- str_remove_all(x, "\\s")
+  }
+
+  x <- str_squish(x)
+}
